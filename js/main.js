3ed52d988bf62cea8252042f73f3654ed2bf4b3e
iceSCREAms <102465510+iceSCREAms@users.noreply.github.com>
2022-03-29 12:16:20 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,70 @@
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
+"use strict"
 
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
+// Меню бургер
 
-        let menuBtn = document.querySelector('.menu__icon');
-        let menu = document.querySelector('.nav');
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
 
-        menuBtn.classList.toggle('active');
-        menu.classList.toggle('active');
-
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-
-
+if (iconMenu) {
+    iconMenu.addEventListener("click", function() {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
     });
-};
+
+}
+
+// Прокрутка при клике на меню
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+// Прокрутка при клике к Блоку gnb от services
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = 100;
+        // const topOffset = 0; // если не нужен отступ сверху
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
